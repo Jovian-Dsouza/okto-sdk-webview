1d26f1ac0439d1da17e2a62c20be211ec492fbc8
Jovian Dsouza <dsouzajovian123@gmai.com>
2024-04-21 22:49:08 +0530

    Add Okto screen

---
+++ src/app/tokens/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Tokens() {
+  return (
+    <div className='flex w-full min-h-screen justify-center items-center text-4xl'>Tokens</div>
+  )
+}
+
+export default Tokens
